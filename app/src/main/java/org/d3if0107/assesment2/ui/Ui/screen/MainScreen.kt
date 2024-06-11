306b5ff5945d70857b209e2730b5073da7c1a3ab
Satrio Wirayuda <oppof5yuda@gmail.com>
2024-06-11 14:42:16 +0700

    Membuat tampilan nama barang

--- app/src/main/java/org/d3if0107/assesment2/ui/Ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0107/assesment2/ui/Ui/screen/MainScreen.kt
@@ -1,8 +1,10 @@
 package org.d3if0107.assesment2.ui.Ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -17,13 +19,17 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -68,19 +74,45 @@ fun ScreenContent(modifier: Modifier) {
 }
 
 @Composable
-fun ListItem(hewan: Hewan){
+fun ListItem(hewan: Hewan) {
     Box(
-        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray)
+        modifier = Modifier
+            .padding(4.dp)
+            .border(1.dp, Color.Gray),
+        contentAlignment = Alignment.BottomCenter
+
     ) {
         AsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
                 .data(HewanApi.getHewanUrl(hewan.imageId))
                 .crossfade(true)
                 .build(),
+
             contentDescription = stringResource(id = R.string.gambar, hewan.nama),
             contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxWidth().padding(4.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
         )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+                .background(Color(red = 0f, green = 0f, blue = 0f, alpha = 0.5f))
+                .padding(4.dp)
+        ) {
+            Text(
+                text = hewan.nama,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Text(
+                text = hewan.namaLatin,
+                fontStyle = FontStyle.Italic,
+                fontSize = 14.sp,
+                color = Color.White
+            )
+        }
     }
 }
 
